739bfabaf5eafa910f8360362315bc6d39a06239
dunnogg <gamemode61@gmail.com>
2024-08-18 17:20:20 +0300

    update getAllTasks function

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   });
   const options = new DocumentBuilder()
     .setTitle('Mining Odyssey API')
-    .setVersion('2.8')
+    .setVersion('3.0')
     .addServer('/', 'Local environment')
     .build();
   const document = SwaggerModule.createDocument(app, options);
