a8aa2e36f8af7e3348b9b208bd3860eda3506dab
dunnogg <gamemode61@gmail.com>
2024-09-27 01:08:47 +0300

    Add start bonus and nickname

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,9 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import * as process from "process";
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    return process.env.DATABASE_NAME;
+    return 'Hello World!';
   }
 }
